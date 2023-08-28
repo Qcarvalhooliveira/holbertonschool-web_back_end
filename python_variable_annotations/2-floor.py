a68a25ba207cd2ba8f8fd736d2764b4caaf14f71
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-28 16:14:51 +0200

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Function floor which takes a float n"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Function that returns the floor of the float"""
+    return math.floor(n)
