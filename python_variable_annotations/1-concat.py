70c385500e9fe28d3e3fbd62c1b96c2ba73b3b39
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-28 15:55:19 +0200

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function concat that takes a string str1 and a string str2"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that returns a concatenated string"""
+    return str1 + str2
