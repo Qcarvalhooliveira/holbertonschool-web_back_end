2b5f6178a62bbc72888efd2e80d1483e5fb8d8c6
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-28 15:39:47 +0200

    Basic annotations - add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Function add that takes a float a and a float b as arguments"""
 
+
 def add(a: float, b: float) -> float:
     """Function that returns their sum as a float"""
     return a + b
