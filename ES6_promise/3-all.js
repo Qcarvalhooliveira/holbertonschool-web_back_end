37519d744cc59bb4b10c6475597634c3c4201a76
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-02 18:50:18 +0200

    Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser} from './utils.js';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+  return Promise
+    .all([photo, user])
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`),
+    () => console.log('Signup system offline'));
+}
+
