2022d31d33c2e576239417a0313998bf14b8a3c6
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-04 14:43:38 +0200

    More mapping

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(list) {
- 
   if (!Array.isArray(list)) {
     return [];
- }
-  return list.map(student => student.id);
+  }
+  const listId = list.map((student) => student.id);
+  return listId;
 }
