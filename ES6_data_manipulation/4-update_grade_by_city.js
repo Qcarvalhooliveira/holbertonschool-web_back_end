a970200c8f0244f9616869233da8a077ad58caef
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-04 15:51:18 +0200

    Combine

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,12 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
-
-  const gradesList = list.filter((student) => student.location === city)
-    .map(student => {
-      const foundGrade = newGrades.find(grade => grade.studentId === student.id);
+  return list.filter((student) => student.location === city)
+    .map((student) => {
+      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
       return {
-	id: student.id,
-	firstName: student.firstName,
-	location: student.location,
-	grade: foundGrade ? foundGrade.grade : 'N/A'
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: foundGrade ? foundGrade.grade : 'N/A',
       };
     });
-
-  return gradesList;
 }
-
