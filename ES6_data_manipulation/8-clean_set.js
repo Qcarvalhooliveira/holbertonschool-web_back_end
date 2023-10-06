ada17a3aa4431c97d0819bc5d3e16df90ae9b13f
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-06 21:56:06 +0200

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
-  const result = [];
   if (startString.length === 0) {
     return '';
   }
+  
+  const result = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
       const restOfString = value.slice(startString.length);
