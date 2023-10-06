64c7d50f919381b1ef1321b03140416bb53692d3
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-06 22:56:53 +0200

    More map data structure

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(map) {
+    if (!(map instanceof Map)) {
+      throw new Error('Cannot process');
+    }
+  
+    map.forEach((quantity, item) => {
+      if (quantity === 1) {
+        map.set(item, 100);
+      }
+    });
+  }
+  
