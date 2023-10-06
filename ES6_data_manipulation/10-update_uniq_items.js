8666ce1c78e75b3df06df345a8ee65d932141717
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-06 22:58:42 +0200

    More map data structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,11 @@
 export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
-    }
-  
-    map.forEach((quantity, item) => {
-      if (quantity === 1) {
-        map.set(item, 100);
-      }
-    });
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
-  
+
+  map.forEach((quantity, item) => {
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  });
+}
