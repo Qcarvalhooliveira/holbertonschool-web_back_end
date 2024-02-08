a7218d849b70be0d8ad5a3f8efede6a9efb79dfa
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-08 17:04:45 +0100

    Creating Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError("put must be implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError("get must be implemented in your cache class")
+    
+class BasicCache(BaseCaching):
+    """ Caching system BasicCache that inherits from BaseCaching.
+    """
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data the item value for the key.
+            If key or item is None, this method should not do anything.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return the value in self.cache_data linked to key.
+            If key is None or if the key doesn’t exist in self.cache_data, return None.
+        """
+        return self.cache_data.get(key, None)
