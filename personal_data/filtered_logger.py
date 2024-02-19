151ba76e5bf5decfb6984a0c40ea95c94814a093
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-19 19:07:29 +0100

    Create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS = ('name','email','phone','ssn','password')
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -37,3 +39,20 @@ class RedactingFormatter(logging.Formatter):
         original_message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             original_message, self.SEPARATOR)
+    
+    def get_logger() -> logging.Logger:
+        """ Function that takes no arguments and returns a logging.Logger.
+        """
+        logger = logging.getLogger("user_data")
+        logger.setLevel(logging.INFO)
+        logger.propagate = False
+
+        stream_handler = logging.StreamHandler()
+        formatter = RedactingFormatter(PII_FIELDS)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        return logger
+
+    
+
