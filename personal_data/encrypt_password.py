4607db21b5b6cdb6844222800a2a437155242755
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-20 16:21:31 +0100

    Encrypting passwords

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Hash_password Module
+"""
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """ Function that hashes a password with a salt.
+    """
+    salt = bcrypt.gensalt()
+    
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
