8aaf17e16fa7e4c391d867ae88897f5d77ae6179
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-20 16:22:52 +0100

    Fixing pycodestyle

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -4,11 +4,12 @@
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """ Function that hashes a password with a salt.
     """
     salt = bcrypt.gensalt()
-    
+
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
