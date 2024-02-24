897b8b3c415abf3b4be53116d7749c34b51d1e32
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-24 15:39:24 +0100

    User ID for Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -13,7 +13,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Create a Session ID for a user_id
+        """ Create a Session ID for a user_id.
         """
         if user_id is None or not isinstance(user_id, str):
             return None
@@ -23,3 +23,11 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a Session ID.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id, None)
