58684c9a842dc911cea4421f00823bea2005e050
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-27 13:49:36 +0100

    test

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,23 +7,24 @@ from user import User
 from sqlalchemy.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password with a randomly-generated salt
+    and returns the salted, hashed password.
+    """
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hashes a password with a randomly-generated salt
-        and returns the salted, hashed password.
-        """
-        salt = bcrypt.gensalt()
-
-        hashed_password = bcrypt.hashpw(password.encode(), salt)
-
-        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """ Registers a user with a given email and password.
@@ -33,6 +34,6 @@ class Auth:
             raise ValueError("User {} already exists".format(email))
 
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
