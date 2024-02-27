deedab3050a13ae54c8af649fb9b7151374bed3d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-27 15:46:18 +0100

    update

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -30,10 +30,13 @@ class Auth:
         """ Registers a user with a given email and password.
         """
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(email))
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(email))
 
         except NoResultFound:
             hashed_password = _hash_password(password)
-            new_user = self._db.add_user(email, hashed_password)
-            return new_user
+            return self._db.add_user(email, hashed_password)
+            
+
+
