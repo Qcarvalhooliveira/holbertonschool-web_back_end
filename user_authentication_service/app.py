235ddd50659fc431310e2c7df7f6ec9d8ff60c8c
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-27 17:19:14 +0100

    Update app.py

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,11 +6,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def welcome():
     """Endpoint returning a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
