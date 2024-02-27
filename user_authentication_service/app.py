165e6e68f0bdead39eed65b849a0b310739b5ce9
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-27 17:18:19 +0100

    Create app.py

---
+++ user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""App module
+"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def welcome():
+    """Endpoint returning a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
