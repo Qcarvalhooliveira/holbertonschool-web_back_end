8634cb63d8d5548a90a00e1232620316e7b9da8c
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-28 17:50:42 +0100

    Log in

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -31,5 +31,23 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """ Endpoint to log in a user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+
+        session_id = AUTH.create_session(email)
+        response = make_response(jsonify({"email": email,
+                                          "message": "logged in"}))
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
