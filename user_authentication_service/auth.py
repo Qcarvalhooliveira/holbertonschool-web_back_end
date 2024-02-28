2bb0dd8ba56fbbca3d7f8704f377461766350694
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-28 15:33:08 +0100

    Fixing import and documentation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,7 +4,7 @@
 import bcrypt
 from db import DB
 from user import User
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -24,10 +24,19 @@ class Auth:
     """
 
     def __init__(self):
+        """ Initializes a new instance of the class.
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
         """ Registers a user with a given email and password.
+
+            Args:
+                email (str): The email address of the new user.
+                password (str): The plaintext password of the new user.
+
+            Returns:
+                User: The instance of the newly created User object.
         """
         try:
             existing_user = self._db.find_user_by(email=email)
@@ -40,8 +49,7 @@ class Auth:
             return new_user
 
     def valid_login(self, email: str, password: str) -> bool:
-        """
-        Validates if a given email and password correspond to a valid user.
+        """ Validates if a given email and password correspond to a valid user.
         """
         try:
             user = self._db.find_user_by(email=email)
