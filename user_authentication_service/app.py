45913094dd0c1abd89adafb0c3200c60b5e8e67a
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-28 18:40:05 +0100

    Log out

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """App module
 """
 
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 AUTH = Auth()
@@ -47,6 +47,23 @@ def login():
         return response
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Endpoint to log out a user.
+    """
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.id)
+        response = make_response(redirect('/'))
+        response.set_cookie('session_id', '', expires=0)
+        return response
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
