590682b167e05c7ae04a8fa2b946e51164070c3f
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-29 10:10:20 +0100

    Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -103,3 +103,15 @@ class Auth:
 
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Updates a user's password based on a reset token.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
+
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
