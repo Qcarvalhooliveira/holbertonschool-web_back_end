705cd7d98b01ecd084d118ef0421b72e2c7f97ea
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-05 15:06:22 +0100

    Mocking a property

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@
 import unittest
 import requests
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import client
 from client import GithubOrgClient
 
@@ -24,6 +24,15 @@ class TestGithubOrgClient(unittest.TestCase):
         gh_client = GithubOrgClient(org)
         self.assertEqual(gh_client.org, expected)
         get_patch.assert_called_once_with(f'https://api.github.com/orgs/{org}')
+
+    def test_public_repos_url(self):
+        """ Test that the _public_repos_url property returns the expected URL.
+        """
+        expected_url = 'https://api.github.com/orgs/google/repos'
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value={'repos_url': expected_url})):
+            gh_client = GithubOrgClient("google")
+            self.assertEqual(gh_client._public_repos_url, expected_url)
 
 
 if __name__ == '__main__':
