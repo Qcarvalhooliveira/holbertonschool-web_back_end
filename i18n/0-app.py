40d72df80429b0f9208a256734c478f782c4af2b
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-07 13:24:07 +0100

    Basic Flask app

---
+++ i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Basic Flask app Module
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """Endpoint returning Hello world.
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
