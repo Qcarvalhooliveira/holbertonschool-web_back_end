a3317839c4034a42e166d0831ea18377e73b0cd0
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-07 14:59:12 +0100

    Fixing config

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -25,7 +25,7 @@ app.config.from_object(Config)
 def welcome() -> str:
     """Endpoint returning Hello world.
     """
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
--- i18n/2-app.py
+++ i18n/2-app.py
@@ -22,16 +22,16 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Select the best match language."""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(["en", "fr"])
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome() -> str:
     """Endpoint returning Hello world.
     """
-    return render_template("0-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
