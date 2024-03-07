d23c2d55bde677a420e38f7169c2770d2c2e3dfa
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-07 19:27:47 +0100

    Parametrize templates

---
+++ i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Basic Flask app Module
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Configuration class.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """Endpoint returning Hello world.
+    """
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Select the best match language."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
