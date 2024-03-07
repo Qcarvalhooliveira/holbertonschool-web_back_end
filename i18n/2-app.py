99c6426d26475f062fa7dc5d1b5c21a5e449e7f7
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-07 15:01:32 +0100

    Update 2-app.py

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -21,17 +21,17 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """Select the best match language."""
-    return request.accept_languages.best_match(["en", "fr"])
-
-
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome() -> str:
     """Endpoint returning Hello world.
     """
     return render_template("2-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Select the best match language."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
