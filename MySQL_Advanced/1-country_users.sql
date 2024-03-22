19f1d76bd56be2726c5e8a410bfedffdbe1311a1
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-22 12:41:01 +0100

    Update 1-country_users.sql

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -2,10 +2,7 @@
 --id, integer, never null, auto increment and primary key
 --email, string (255 characters), never null and unique
 --name, string (255 characters)
---country, enumeration of countries: US, CO and TN, never null 
---(= default will be the first element of the enumeration, here US)
---If the table already exists, the script should not fail
---The script can be executed on any database
+--country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
