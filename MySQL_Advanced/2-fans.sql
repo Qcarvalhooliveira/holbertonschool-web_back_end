50b3ac409bbfa7d57773ffed88ec574ce8fc5caf
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-22 18:36:25 +0100

    Create 2-fans.sql

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+--Script that ranks country origins of bands, ordered by the number of (non-unique) fans
+
+SELECT origin, SUM(fans) nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
