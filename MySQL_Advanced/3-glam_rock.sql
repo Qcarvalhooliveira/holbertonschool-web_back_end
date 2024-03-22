65a5b9fb60348d19312c8d3f2ca54b3f448ba503
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-22 20:56:16 +0100

    Update 3-glam_rock.sql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
---Script that lists all bands with Glam rock AS their main style, ranked by their longevity
+-- Script that lists all bands with Glam rock AS their main style, ranked by their longevity
 
 SELECT  band_name,(ifnull(split,2020) - ifnull(formed,0)) lifespan
 FROM metal_bands
