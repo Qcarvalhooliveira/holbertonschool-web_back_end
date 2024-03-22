60156380f96050f3044b55e29baa8c9eb1c5391d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-22 18:26:10 +0100

    Update 1-country_users.sql

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
---Creates a table users following these requirements:
---id, integer, never null, auto increment and primary key
---email, string (255 characters), never null and unique
---name, string (255 characters)
---country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
+-- Creates a table users following these requirements:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
