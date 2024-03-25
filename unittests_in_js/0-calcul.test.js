8236afc98135297bd08bab09b0566f51ce99607d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-25 21:25:46 +0100

    Update 0-calcul.test.js

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,9 +4,8 @@ const calculateNumber = require('./0-calcul');
 describe('Test the calculateNumber sum', function() {
     it('should return the sum of rounded a and b', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
-    });
-
-    it('should correctly round a and b upwards', function() {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
