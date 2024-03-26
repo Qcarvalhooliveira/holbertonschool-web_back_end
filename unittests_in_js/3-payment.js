b23a5e85523f88b0c4a8a6b65e4248febfd8caa8
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-26 15:05:08 +0100

    Update 3-payment.js

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
     const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is:${res}`);
+    console.log(`The total is: ${res}`);
 }
 
 module.exports = sendPaymentRequestToApi;
