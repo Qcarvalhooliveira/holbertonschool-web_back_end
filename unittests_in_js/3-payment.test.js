3ed6eb88589fedb82fff44f0e64616ae39ae7b5d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-26 11:58:37 +0100

    Fixing call function

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -10,6 +10,7 @@ describe('sendPaymentRequestToApi', function() {
 
         sendPaymentRequestToApi(100, 20);
 
+        expect(spyUtils.calledOnce).to.be.true;
         expect(spyUtils.calledWith('SUM', 100, 20)).to.be.true;
         expect(spyConsole.calledWith('The total is: 120')).to.be.true;
 
