b70b7d816cbfa92ef3ef059a78812b52ff7900b9
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-28 21:42:55 +0100

    Node Redis client and advanced operations

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import redis from 'redis';
+import { print } from 'redis';
+const client = redis.createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', function(err) {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.hset('HolbertonSchools', 'Portland', 50, print);
+client.hset('HolbertonSchools', 'Seattle', 80, print);
+client.hset('HolbertonSchools', 'New York', 20, print);
+client.hset('HolbertonSchools', 'Bogota', 20, print);
+client.hset('HolbertonSchools', 'Cali', 40, print);
+client.hset('HolbertonSchools', 'Paris', 2, print);
+
+client.hgetall('HolbertonSchools', (err, result) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(result);
+});
