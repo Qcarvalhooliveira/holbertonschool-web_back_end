77cb4e7568eb4f67bd8c45e75df5eb074ac54f70
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-03-29 14:46:36 +0100

    Track progress and errors with Kue: Create the Job processor

--- queuing_system_in_js/7-job_creator.js
+++ queuing_system_in_js/7-job_creator.js
@@ -49,21 +49,21 @@ const jobs = [
     }
   ];
 
-  jobs.forEach((jobData, index) => {
+  jobs.forEach((jobData) => {
   const job = queue.create('push_notification_code_2', jobData)
   .save((err) => {
     if (!err) console.log(`Notification job created: ${job.id}`);
     });
 
-job.on('complete', () => {
-  console.log('Notification job ${job.id} completed');
+    job.on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
     });
 
-job.on('failed', (errorMessage) => {
-  console.log('Notification job ${job.id} failed: ${errorMessage}');
+    job.on('failed', (errorMessage) => {
+        console.log(`Notification job ${job.id} failed: ${errorMessage}`);
     });
 
-job.on('progress', (progress) => {
-    console.log('Notification job ${job.id} ${progress}% complete');
+    job.on('progress', (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
     });
 });
---
+++ queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,22 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const blacklist = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+
+  if (blacklist.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
+  }
+}
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
